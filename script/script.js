5f6e228973767c4e8a2eccaef13b792fb904cd12
Vitalii <vetal_mamchur@ukr.net>
2022-10-20 00:46:48 +0300

    Add Script

--- script/script.js
+++ script/script.js
@@ -3,4 +3,17 @@ let menuListBlock = document.querySelector('.menuListBlock');
 
 mobileNavBar.addEventListener('click', function(){
     menuListBlock.classList.toggle('menuListHeight');
+});
+
+
+
+let newArrivalsPriceBlock = document.querySelector('.newArrivalsPriceBlock');
+let seeMore = document.querySelector('.seeMoreBtn');
+let arrivalsBlock = document.querySelector('.newArrivalsBlock');
+
+seeMore.addEventListener('click', function(){
+    document.querySelector('.seeMoreBlock').style.display = 'none';
+    arrivalsBlock.style.height = 'auto';
+    let cloneNewArrivals = newArrivalsPriceBlock.cloneNode(true);
+    newArrivalsPriceBlock.after(cloneNewArrivals);
 })
